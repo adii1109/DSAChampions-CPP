0754e9e9182f3d61ca2fc2a23f09121f542a41f2
Aditya <asp1192002@gmail.com>
2023-09-02 22:28:15 +0530

    Basic

---
+++ Basic/Patterns-2.cpp
@@ -0,0 +1,180 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+
+    //1]
+    // 1
+    // 23
+    // 456
+    // 78910
+
+    // int n;
+    // cin>>n;
+
+    // int count=1;
+
+    // for(int row=1;row<=n;row++){
+    //     for(int col=1;col<=row;col++){
+    //         cout<<count;
+    //         count++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    //2]
+
+    
+    // 1
+    // 21
+    // 321
+    // 4321
+
+    // int n;
+    // cin>>n;
+
+    // for(int row=1;row<=n;row++){
+    //     for(int col=row;col>=1;col--){
+    //         cout<<col;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    //3}
+
+
+    // AAAAA
+    // BBBBB
+    // CCCCC
+    // DDDDD
+    // EEEEE
+
+
+    // int n;
+    // cin>>n;
+
+    // for(int row=1;row<=n;row++){
+    //     for(int col=1;col<=n;col++){
+    //         char ch='A'+(row-1);
+    //         cout<<ch;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+//4]
+
+    // ABCDEF
+    // ABCDEF
+    // ABCDEF
+    // ABCDEF
+    // ABCDEF
+    // ABCDEF   
+
+    // int n;
+    // cin>>n;
+
+
+    // for(int row=1;row<=n;row++){
+    //     for(int col=1;col<=n;col++){
+    //         char ch='A'+(col-1);
+    //         cout<<ch;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+//5]
+
+
+    // ABCDE
+    // FGHIJ
+    // KLMNO
+    // PQRST
+    // UVWXY
+
+
+    // int n;
+    // cin>>n;
+
+    // char ch='A';
+
+    // for(int row=1;row<=n;row++){
+    //     for(int col=1;col<=n;col++){
+    //         cout<<ch;
+    //            ch+=1;
+    //     }
+     
+    //     cout<<endl;
+    // }
+
+
+//6]
+
+
+    // ABC
+    // BCD
+    // CDE
+
+    // int n;
+    // cin>>n;
+    
+    // for(int row=1;row<=n;row++){
+    //     char ch='A'+(row-1);
+    //     for(int col=1;col<=n;col++){
+            
+    //         cout<<ch;
+    //         ch++;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+//7]
+
+    // A
+    // BC
+    // CDE
+    // DEFG
+
+    // int n;
+    // cin>>n;
+
+
+    // for(int row=1;row<=n;row++){
+    //    char ch='A'+(row-1);
+    //     for(int col=1; col<=row;col++){
+    //          cout<<ch;
+    //          ch++;}
+    //          cout<<endl;
+    // }
+
+
+
+//8]
+
+    // D
+    // CD
+    // BCD
+    // ABCD
+
+    // int n;
+    // cin>>n;
+
+    // for (int row=1;row<=n;row++){
+    //     for(int col=1;col<=row;col++){
+    //         char ch=(('A'-(row-col))+(n-1));
+    //         cout<<ch;
+    //     }
+    //     cout<<endl;
+    // }
+
+    return 0;
+}
